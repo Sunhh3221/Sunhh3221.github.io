3072072f1cb318c8764b924886192b16bb7e81db
hang.sun <hang.sun@gareatech.com>
2025-06-12 17:27:33 +0800

    Site updated: 2025-06-12 17:27:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/30/hello-world/"];function toRandomPost(){
+var posts=["2025/06/12/Java8-接口的默认方法和静态方法/","2025/04/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
